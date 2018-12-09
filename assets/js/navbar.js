200620bcc5cbb3a944d32773ea06d87d9538748b
Axle10 <saptarshi.sinha.ind@gmail.com>
2018-12-09 19:22:00 +0530

    homepage modified using materialize and bootstrap

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -4,75 +4,28 @@ It contains the following functions:
  2. Changing the navbar color on scrolling
  3. Changing the menu color on scrolling
  4. Shrinking the navbar on scrolling
+
+ function 1 and 4 not working. Check css too.
 */
 
 $(document).ready(function ()
 {
-    //function on scroll
-    $(document).on("scroll", onScroll);
-
-    //smoothscroll or click
-    $('a[href^="#"]').on('click', function (e)
+  $(".button-collapse").sideNav();
+  $(window).scroll(function(){
+    if($(window).scrollTop()>0)//scrolling is more than zero
     {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('a').each(function ()
-        {
-            $(this).removeClass('active');
-        });
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate(
-        {
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function ()
-        {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
-});
-
-function onScroll(event)
-{
-    var scrollPos = $(document).scrollTop();
-    if (scrollPos > 0)
-    {
-      //change navigation bar color from transparent to white on scrolling
-      $("#navigationbar").css("background" , "white");
-      //$("#navigationbar").css("opacity" , "1");
-      //Shrink the navigation bar on scrolling
-      $(".navbar").addClass("shrink");
-      //change the font color to black
-      //$(".nav-link").css("color","black");
+      $('.nav-wrapper').addClass('white');
+      $('#navmenu li a span').css("color", "black");
+      $('.nav-wrapper').addClass('shrink');
     }
     else
     {
-      //Change navigation bar color to transparent when scroll is zero
-      $("#navigationbar").css("background-color" , "transparent");
-      //$("#navigationbar").css("opacity" , "0.5");
-      //Keep the navbar to its actual size
-      $("#navigationbar").removeClass("shrink");
-      //change the font color to white
-      //$(".nav-link").css("color","white");
+      $('.nav-wrapper').removeClass('white');
+      $('#navmenu li a span').css("color","white");
+      $('.nav-wrapper').removeClass('shrink');
     }
-    //change the active menu on the navbar as required
-    $('#navbartoggler a').each(function ()
-    {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos)
-        {
-            $('#navbartoggler ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else
-        {
-            currLink.removeClass("active");
-        }
-    });
-}
+  });
+
+  //CAROUSEL
+  $('.carousel').carousel();
+});
